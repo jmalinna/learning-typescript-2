6611cb24940543b778cc170891282769556765e4
Julia Malinina <malinina.artwork@gmail.com>
2022-11-10 23:54:08 +0100

    Add TodoList.tsx Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 function App() {
+  const todoItems = [{ id: 't1', text: 'Finish the course'}];
+
   return (
     <div className="App">
+      <TodoList todoItems={todoItems} />
     </div>
   );
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface TodoListProps {
+  todoItems: { id: string, text: string }[];
+}
+
+const TodoList = ({ todoItems }: TodoListProps) => {
+  return (
+    <ul>
+      {todoItems.map(item => <li key={item.id}>(item.text)</li>)}
+    </ul>
+  )
+};
+export default TodoList;
